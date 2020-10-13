951c16e8d2e6b21a128fc7107a7ef8fae9fbfe88
sebastiaan <sebastiaan@pop-os.localdomain>
2020-10-13 23:52:58 +0200

    error handling added

--- TMDB.py
+++ TMDB.py
@@ -2,9 +2,10 @@ import urllib.parse
 import requests 
 
 # enter your API-Key here
-auth = "INSERT API-KEY HERE"
+auth = "INSET-API-KEY-HERE"
 # the base URL for api requests
 urlbase = "https://api.themoviedb.org/3/search/movie?"
+
 
 # while loop to allow for user input and handeling of request
 while  True:
@@ -12,21 +13,28 @@ while  True:
     #allows user to exit the program
     if query == "q" or query == "quit":
         break
-    url = urlbase +urllib.parse.urlencode({"api_key":auth, "query": query})
+    #auth v3
+    url = urlbase +urllib.parse.urlencode({"api_key": "%s" %auth, "query": query})
+    #auth v4
+    #url = urlbase +urllib.parse.urlencode({"Authentication ": "Bearer %s" %auth, "query": query})
     json_data = requests.get(url).json()
     print("URL: " + (url))
-
-    
-    resultcnt = 0
-    for r in (json_data["results"]):
-        #filters out entries with a vote average of 0 
-        if r["vote_average"] > 0:
-            print("=============================================")
-            print ("title: \"" + r["title"] + "\"")
-            print ("release date: " + r["release_date"])
-            print ("average rating: " + str(r["vote_average"]) )
-            resultcnt += 1
-    print("=============================================")
-    print("---------------------------------------------")        
-    print (str(resultcnt) + " results shown.")
-    print("---------------------------------------------")
+    #checks if there is an error code
+    if "status_code" not in json_data:
+        resultcnt = 0
+        for r in (json_data["results"]):
+           #filters out entries with a vote average of 0 
+           if r["vote_average"] > 0:
+               print("=============================================")
+               print ("title: \"" + r["title"] + "\"")
+               print ("release date: " + r["release_date"])
+               print ("average rating: " + str(r["vote_average"]) )
+               resultcnt += 1
+        print("=============================================")
+        print("---------------------------------------------")        
+        print (str(resultcnt) + " results shown.")
+        print("---------------------------------------------")
+    else:
+        #prints error message when applicable 
+        json_status = json_data["status_message"]
+        print ("error: " + json_status)
